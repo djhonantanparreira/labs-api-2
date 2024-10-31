f44ce1863f88ffcdbcb96483e1cdee38f6e227ee
Djhonantan Parreira <djhonantanedu@gmail.com>
2024-10-31 19:35:10 -0300

    Atualiza AuthController para incluir usuário na resposta de login e corrige a validação no CreateUserController

--- app/Http/Controllers/User/CreateUserController.php
+++ app/Http/Controllers/User/CreateUserController.php
@@ -24,11 +24,11 @@ class CreateUserController extends Controller
 
         $request->validate([
 
-            'name' => ['required|string|max:60'],
-            'email' => ['required|email|unique:users'],
+            'name' => ['required', 'string', 'max:60'],
+            'email' => ['required', 'email', 'unique:users'],
             'linkedin' => ['required'],
-            'password' => ['required|min:8'],
-            'register_token' => ['required|string']
+            'password' => ['required', 'min:8'],
+            'register_token' => ['required', 'string']
 
         ], [
             'name.required' => 'O campo nome é obrigatório.',
@@ -60,7 +60,6 @@ class CreateUserController extends Controller
             return response()->json([
                 'message' => 'Usuário cadastrado com sucesso.',
             ], 201);
-
         } else {
 
             return response()->json([
